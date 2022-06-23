852c52babf0b408300a5eddc983e3e02527f28f4
Rings-Rachit <86543517+Rings-Rachit@users.noreply.github.com>
2022-06-23 12:55:38 -0700

    Update main.js

--- main.js
+++ main.js
@@ -42,7 +42,7 @@ function gotPoses(results)
      {
           console.log(results);
           noseX=results[0].pose.nose.x-15;
-          noseY=results[0].pose.nose.y-15;
+          noseY=results[0].pose.nose.y-5;
           console.log("Nose x is = "+ noseX);
           console.log("Nose y is = " +noseY);
           
